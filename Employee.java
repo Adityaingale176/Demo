767a4d8cbb5e5da40747972536d28f2fc38ee506
Adityaingale176 <105264515+Adityaingale176@users.noreply.github.com>
2022-05-31 21:36:05 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -3,7 +3,7 @@ public class Employee {
     public static void main (String ){
       
       System.out.println("In employee");
-      
+      System.out.println("Pls approve the coode")
       
     }
 }
